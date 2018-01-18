785db677c9d857945e388dcde7d0178c0641e528
AndrewMElliott <32204180+AndrewMElliott@users.noreply.github.com>
2018-01-17 21:45:06 -0500

    git init

---
+++ Assets/BallScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D col){
+		GameObject e = Instantiate (explosion, this.transform.position, Quaternion.identity);
+		Destroy (e, 2f);
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/FireCannon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCannon : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject ball;
+	[SerializeField]
+	private float offset;
+	[SerializeField]
+	private float force;
+	[SerializeField]
+	private GameObject firePoint;
+	[SerializeField]
+	private float rotationSpeed;
+	private float fireRate = 1f;
+	private float coolDown = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && Time.time > coolDown) {
+			GameObject b = Instantiate (ball, firePoint.transform.position, this.transform.rotation);
+			Rigidbody2D phy = b.GetComponent<Rigidbody2D> ();
+			phy.AddForce (b.transform.up * force);
+			coolDown = Time.time + fireRate;
+		}
+		if(Input.GetAxis("Vertical") > 0){
+			Quaternion rotation = this.transform.rotation;
+			rotation *= Quaternion.Euler (0f, 0f, rotationSpeed);
+			this.transform.rotation = rotation;
+
+		}
+		if(Input.GetAxis("Vertical") < 0){
+			Quaternion rotation = this.transform.rotation;
+			rotation *= Quaternion.Euler (0f, 0f, -rotationSpeed);
+			this.transform.rotation = rotation;
+
+		}
+	}
+}
